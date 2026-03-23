dbea5b70237ebe7678a8d4991c26011a9d6c6014
Hetvi <131541036+Hetvikandolia@users.noreply.github.com>
2026-03-23 18:39:25 +0530

    new files added

---
+++ 01_Patterns/02right_tri_star.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    // printf("right tri ");
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+}
---
+++ 01_Patterns/04right_tri_digit2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    // printf("right tri ");
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
+// 1
+// 22
+// 333
+// 4444
+// 55555
