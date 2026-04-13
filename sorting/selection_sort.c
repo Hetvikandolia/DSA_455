fbbbfd78dff9187eeb24a6fd81881b8f4008a5f1
Hetvi <131541036+Hetvikandolia@users.noreply.github.com>
2026-04-13 22:16:46 +0530

    commit baki

--- sorting/selection_sort.c
+++ sorting/selection_sort.c
@@ -1,40 +1,49 @@
+// #include<stdio.h>
+
+// void swap (int *x,int *y)
+// {
+// 	int temp=*x;
+// 	*x=*y;
+// 	*y=temp;
+// }
+
+// void selectionsort(int arr[],int n)
+// {
+//     int min_idx,j,i;
+//     for(i=0;i<n-1;i++)
+//         {
+//             min_idx=i;
+// 		    for(j=i+1;j<n;j++)
+//                 if(arr[j]<arr[min_idx])
+//                     min_idx=j;
+//                     if(min_idx != i) ///bhuuuullll
+// 				    swap(&arr[min_idx],&arr[i]);
+//         }
+// }
+
+// int main()
+// {
+// 	int arr[]={5,4,7,8,9,1};
+// 	int n=sizeof(arr)/sizeof(arr[0]);
+
+// 	printf("before sorting: \n");
+// 	for(int i=0;i<n;i++)
+// 		printf("%d ",arr[i]);		
+
+// 	selectionsort(arr,n);
+// 	printf("\n");
+// 	printf("sorted array :  \n");
+
+// 		for(int i=0;i<n;i++)
+// 		printf("%d ",arr[i]);		
+// 	return 0;
+// }
+
+
 #include<stdio.h>
+int main(){
+	int n[5]={10,50,70,80,20};
+	int size=sizeof(n)/sizeof(n[0]);
 
-void swap (int *x,int *y)
-{
-	int temp=*x;
-	*x=*y;
-	*y=temp;
-}
-
-void selectionsort(int arr[],int n)
-{
-    int min_idx,j,i;
-    for(i=0;i<n-1;i++)
-        {
-            min_idx=i;
-		    for(j=i+1;j<n;j++)
-                if(arr[j]<arr[min_idx])
-                    min_idx=j;
-                    if(min_idx != i) ///bhuuuullll
-				    swap(&arr[min_idx],&arr[i]);
-        }
-}
-
-int main()
-{
-	int arr[]={5,4,7,8,9,1};
-	int n=sizeof(arr)/sizeof(arr[0]);
-
-	printf("before sorting: \n");
-	for(int i=0;i<n;i++)
-		printf("%d ",arr[i]);		
-
-	selectionsort(arr,n);
-	printf("\n");
-	printf("sorted array :  \n");
-
-		for(int i=0;i<n;i++)
-		printf("%d ",arr[i]);		
-	return 0;
+	
 }
