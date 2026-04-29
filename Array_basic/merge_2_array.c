68a9c82ebde84c2ca06f1271be181a97184c1052
Hetvi <131541036+Hetvikandolia@users.noreply.github.com>
2026-04-29 21:46:39 +0530

    29/4 done

--- Array_basic/merge_2_array.c
+++ Array_basic/merge_2_array.c
@@ -1,23 +1,45 @@
 #include<stdio.h>
 int main(){
-    int n[5]={10,5,8,2,30};
+    int n[8]={10,5,8,2,2,2,30};
     int size1=sizeof(n)/sizeof(n[0]);
 
-    int m[5];
-    // ={10,55,88,22,30};
-    // int size2=sizeof(n)/sizeof(n[0]);
+    int m[3]={10,888,6};
+    int size2=sizeof(m)/sizeof(m[0]);
 
-    for (int i = 0; i < size1; i++)
+    int ans[10];
+    int k=0;
+    
+    for (int  i = 0; i < size1; i++)
     {
-        for (int j = 0; j < size1; i++)
+        int found=0;
+        for (int j = 0; j < k; j++)
         {
-            int temp=n[i];
-        n[i]=n[j];
-        n[j]=temp;  
+            if (n[i]==ans[j])
+            {
+                found=1;
+                break;
+            }
         }
-        printf()
-        
+        if(!found){ans[k] = n[i]; k++; } 
     }
     
+
+    for (int  i = 0; i < size2; i++)
+    {
+        int found=0;
+        for (int j = 0; j < k; j++)
+        {
+            if (m[i]==ans[j])
+            {
+                found=1;
+                break;
+            }
+        }
+        if(!found){ans[k] = m[i]; k++; } 
+    }
+
+    for (int i = 0; i < k; i++)
+    {
+        printf("%d ",ans[i]); }
     
 }
