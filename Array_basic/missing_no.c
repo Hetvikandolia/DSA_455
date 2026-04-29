93d3b9936f1da2f13d705486fbf94d3993838342
Hetvi <131541036+Hetvikandolia@users.noreply.github.com>
2026-04-29 22:06:04 +0530

    29/4

---
+++ Array_basic/missing_no.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int n[8]={8, 2, 4, 5, 3, 7, 1};
+    int size=sizeof(n)/sizeof(n[0]);
+    int k=8;
+    for (int i = 1; i <= size; i++)
+    {
+        int found=0;
+        for (int j = 0; j < size-1; j++)
+        {
+            if(n[j]==i){
+            found=1;
+            break;}
+        }
+        if(!found){printf("%d",i);}
+        
+    }
+    
+}
