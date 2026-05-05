20799c3426dbf1ea57d895115d16eca481d1bbaf
Hetvi <131541036+Hetvikandolia@users.noreply.github.com>
2026-05-05 22:09:27 +0530

    5/5

--- Array_basic/missing_no.c
+++ Array_basic/missing_no.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main(){
-    int n[8]={8, 2, 4, 5, 3, 7, 1};
+    int n[8]={8, 2, 4, 6, 3, 7, 1};
     int size=sizeof(n)/sizeof(n[0]);
     int k=8;
+    
     for (int i = 1; i <= size; i++)
     {
         int found=0;
