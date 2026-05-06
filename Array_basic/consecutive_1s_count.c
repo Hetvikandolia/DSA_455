ed330ace3cfebe80ffde034de42521471857f3f0
Hetvi <131541036+Hetvikandolia@users.noreply.github.com>
2026-05-06 22:31:42 +0530

    6/5

---
+++ Array_basic/consecutive_1s_count.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    int n[6]={1,1,1,0,1,1};
+    int size=sizeof(n)/sizeof(n[0]);
+
+    int count=0;
+    int maxcount=0;
+    for (int i = 0; i < size; i++)
+    {
+        
+        if(n[i]==1){count++; }
+        else{count=0;}
+
+        if(count>maxcount){maxcount = count;}
+    }
+    
+
+    
+        printf("%d",maxcount);
+
+    
+}
